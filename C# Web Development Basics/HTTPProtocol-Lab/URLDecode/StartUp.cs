9edd73e601ec4a399f02dddcdbe306ef9114ded9
kamen <smallblind>
2018-05-18 12:36:35 +0300

    C#  Web Development Basics

---
+++ C# Web Development Basics/HTTPProtocol-Lab/URLDecode/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLDecode
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var encodeUrl = Console.ReadLine();
+
+            var decodeUrl = WebUtility.UrlDecode(encodeUrl);
+
+            Console.WriteLine(decodeUrl);
+        }
+    }
+}
