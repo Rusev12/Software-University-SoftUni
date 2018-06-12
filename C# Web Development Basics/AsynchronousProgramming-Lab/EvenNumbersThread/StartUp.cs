e6d30771f440629f314beb56cbb2d3a461c5e82b
kamen <smallblind>
2018-06-12 16:03:15 +0300

    C# Web - WebServer, Bootstrap and AsynchronousProgramming

---
+++ C# Web Development Basics/AsynchronousProgramming-Lab/EvenNumbersThread/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace EvenNumbersThread
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+
+            var events = new Thread(() => PrintEvenNumbers(start, end));
+            events.Start();
+            events.Join();
+            Console.WriteLine("Thread finished work");
+        }
+
+        public static void PrintEvenNumbers(int start , int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
